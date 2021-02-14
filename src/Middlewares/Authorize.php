a04f4b9bb0ebe0feeb63bd0a8055ee27c5cbcb86
Anecha Sun <Anachitheu.k@gmail.com>
2021-02-15 04:00:27 +0700

    Update auth middleware

--- src/Middlewares/Authorize.php
+++ src/Middlewares/Authorize.php
@@ -3,29 +3,46 @@
 namespace App\Middlewares;
 
 use Firebase\JWT\JWT;
-use Firebase\JWT\JWT\ExpiredException;
 use App\Models\User;
 use Exception;
+use DomainException;
+use UnexpectedValueException;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\SignatureInvalidException;
 
 class Authorize {
 
     public function __invoke($req, $res, $next) {
+        $pattern = '/^Bearer:(\s){1,}/';
         $response = [
             'message' => 'Unauthorized'
         ];
         
         $token = $req->getHeaderLine('authorization');
 
+        
         if (!$token) {
             return $res->withJson($response, 401);
         }
 
+        if ($token && !preg_match($pattern, $token)) {
+            return $res->withJson(['message' => 'Invalid token'], 401);
+        }
+
         try {
-            $token = preg_replace('/^bearer:(\s+)?/i', '', $token);
-            $tokenDecoded = JWT::decode($token, getenv("SECRET_KEY"), array('HS256'));     
+            $token = preg_replace($pattern, '', $token);
+            $tokenDecoded = JWT::decode($token, getenv("JWT_SECRET"), array('HS256'));     
         } catch (Exception $e) {
             if ($e instanceof ExpiredException) {
-                return $res->withJson(['message' => 'Token expired'], 401);
+                return $res->withJson(['message' => 'Expired token'], 401);
+            }
+
+            if (
+                $e instanceof DomainException ||
+                $e instanceof SignatureInvalidException ||
+                $e instanceof UnexpectedValueException
+            ) {
+                return $res->withJson(['message' => 'Invalid token'], 401);
             }
 
             return $res->withJson($response, 401);
@@ -36,7 +53,7 @@ class Authorize {
             return $res->withJson($response, 401);
         }
 
-        $req = $req->withAttribute('user', $user);
+        $req = $req->withAttribute('user', $user->toArray());
 
         return $next($req, $res);
     }
