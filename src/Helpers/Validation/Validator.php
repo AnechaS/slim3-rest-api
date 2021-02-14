be0344df1c6fb0929663bec2adf47564ab67e2d5
Anecha Sun <Anachitheu.k@gmail.com>
2021-02-15 04:02:23 +0700

    Update validation helper

--- src/Helpers/Validation/Validator.php
+++ src/Helpers/Validation/Validator.php
@@ -3,31 +3,25 @@
 namespace App\Helpers\Validation;
 
 use Illuminate\Validation;
-use Illuminate\Validation\DatabasePresenceVerifier;
 use Illuminate\Translation;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Database\ConnectionResolver;
 
+use App\Helpers\Translation\Translator;
 
 class Validator {
 
     static public function make(array $data, array $rules, array $messages = [], array $customAttributes = []) {
-        $path = dirname(__DIR__, 2) . '/Lang';
-        $filesystem = new Filesystem();
-        $filesLoader = new Translation\FileLoader($filesystem,  $path);
-        $filesLoader->addNamespace('lang', $path);
-        $filesLoader->load('en', 'validation', 'lang');
-        $translator = new Translation\Translator($filesLoader, 'en');
-        $validation = new Validation\Factory($translator);
+        $validation = new Validation\Factory(new Translator);
 
+        // Todo not use variable global in method.
         global $capsule;
         $connection = $capsule->getConnection();
         $db = new ConnectionResolver(['default' => $connection]);
         $db->setDefaultConnection('default');
-        $validation->setPresenceVerifier(new DatabasePresenceVerifier($db));
-        
-        $validator = $validation->make($data, $rules, $messages, $customAttributes);
+        $validation->setPresenceVerifier(new Validation\DatabasePresenceVerifier($db));
 
+        $validator = $validation->make($data, $rules, $messages, $customAttributes);
         return $validator;
     }
 }
